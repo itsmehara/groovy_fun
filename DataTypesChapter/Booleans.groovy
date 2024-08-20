86cbbd8706e20193dabb692b8db624f4e3ddb81b
Its Me Hara <17094719+itsmehara@users.noreply.github.com>
2024-08-20 23:32:07 +0530

    Create Booleans.groovy

---
+++ DataTypesChapter/Booleans.groovy
@@ -0,0 +1,13 @@
+// Booleans.groovy
+
+// Boolean values
+def isTrue = true
+def isFalse = false
+
+println "Boolean true: ${isTrue}"
+println "Boolean false: ${isFalse}"
+
+// Logical operations
+println "Not isTrue: ${!isTrue}"
+println "isTrue AND isFalse: ${isTrue && isFalse}"
+println "isTrue OR isFalse: ${isTrue || isFalse}"
