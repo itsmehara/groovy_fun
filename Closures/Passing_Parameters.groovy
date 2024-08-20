09b6a57ad9ce95fe531bbee4993f35f704fe2c3e
Its Me Hara <17094719+itsmehara@users.noreply.github.com>
2024-08-20 23:46:25 +0530

    Create Passing_Parameters.groovy

---
+++ Closures/Passing_Parameters.groovy
@@ -0,0 +1,9 @@
+// Passing_Parameters.groovy
+
+// Define a closure that takes parameters
+def greetPerson = { name ->
+    println "Hello, ${name}!"
+}
+
+// Call the closure with a parameter
+greetPerson("Alice")
