606399fb1d9fc526cb56bd946292c2d09d87a795
Its Me Hara <17094719+itsmehara@users.noreply.github.com>
2024-08-20 23:21:34 +0530

    Create Variables_and_Types.groovy

---
+++ BasicChapter/Variables_and_Types.groovy
@@ -0,0 +1,16 @@
+// Variables_and_Types.groovy
+
+// Declare variables with different types
+def name = "Alice"     // String
+def age = 30           // Integer
+def height = 5.7       // Float
+def isStudent = true  // Boolean
+
+// Print the variables
+println "Name: ${name}"
+println "Age: ${age}"
+println "Height: ${height}"
+println "Is Student: ${isStudent}"
+
+// Type checking
+println "Type of age: ${age.getClass().getName()}"
