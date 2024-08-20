e84b7866f7c72974714367be4fa072bc31fab821
Its Me Hara <17094719+itsmehara@users.noreply.github.com>
2024-08-20 23:29:25 +0530

    Create Strings_and_GStrings.groovy

---
+++ DataTypesChapter/Strings_and_GStrings.groovy
@@ -0,0 +1,16 @@
+// Strings_and_GStrings.groovy
+
+// String
+def name = "Alice"
+println "String: ${name}"
+
+// GString
+def age = 30
+def message = "Name: ${name}, Age: ${age}"
+println "GString: ${message}"
+
+// Multiline string
+def multilineString = """This is a 
+multiline string
+in Groovy."""
+println "Multiline String: ${multilineString}"
