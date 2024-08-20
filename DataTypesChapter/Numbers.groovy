48cf09d200cae836ad59846493d62dd70eaeca0d
Its Me Hara <17094719+itsmehara@users.noreply.github.com>
2024-08-20 23:31:33 +0530

    Create Numbers.groovy

---
+++ DataTypesChapter/Numbers.groovy
@@ -0,0 +1,21 @@
+// Numbers.groovy
+
+// Integer
+def intValue = 42
+println "Integer: ${intValue}"
+
+// Float
+def floatValue = 3.14
+println "Float: ${floatValue}"
+
+// Double
+def doubleValue = 2.71828
+println "Double: ${doubleValue}"
+
+// BigInteger
+def bigIntValue = 123456789012345678901234567890
+println "BigInteger: ${bigIntValue}"
+
+// BigDecimal
+def bigDecimalValue = 12345.678901234567890
+println "BigDecimal: ${bigDecimalValue}"
