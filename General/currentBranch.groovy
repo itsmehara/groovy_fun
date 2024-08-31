6e2d84ba5a1bdd6524d27efae1a49813757e7b05
Its Me Hara <17094719+itsmehara@users.noreply.github.com>
2024-08-31 11:02:41 +0530

    Update currentBranch.groovy

--- General/currentBranch.groovy
+++ General/currentBranch.groovy
@@ -1,1 +1,14 @@
 
+// Read current branch name from current process of git.
+
+def getCurrentGitBranchV1() {
+    def branchName = new File('.git/HEAD').text.trim().split(':')[1].trim()
+    return branchName
+}
+
+// Or using the git command:
+
+def getCurrentGitBranchV2() {
+    def branchName = 'git rev-parse --abbrev-ref HEAD'.execute().text.trim()
+    return branchName
+}
